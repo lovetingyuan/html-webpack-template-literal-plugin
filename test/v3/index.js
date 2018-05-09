6ba0a99fee93e845b2bbcc58acaef63769cac15a
Baitu Huang <baitu.huang@shopeemobile.com>
2018-05-09 14:13:35 +0800

    make it compatible to Webpack v4

--- test/v3/index.js
+++ test/v3/index.js
@@ -3,7 +3,7 @@ var test = require('tape');
 var path = require('path');
 var fs = require('fs');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var TemplateLiteralCompile = require('../index');
+var TemplateLiteralCompile = require('../../index');
 
 var dist = path.resolve(__dirname, 'dist');
 var htmlTitle = 'this is title ' + Math.random();
@@ -19,7 +19,7 @@ test('test simple webpack config', function (t) {
     plugins: [
       new HtmlWebpackPlugin({
         inject: true,
-        template: '!!html-loader!test/index.html',
+        template: '!!html-loader!index.html',
         templateData: {
           title: htmlTitle
         }
@@ -29,6 +29,7 @@ test('test simple webpack config', function (t) {
   };
   webpack(config, function(err, stats) {
     t.equal(err, null);
+    debugger;
     t.equal(stats.hasErrors(), false);
     var htmlContent = fs.readFileSync(path.join(dist, 'index.html'), 'utf8');
     var ret = htmlContent.match(/<title>(.+)<\/title>/);
---
+++ test/v4/index.js
@@ -0,0 +1,42 @@
+var webpack = require('webpack');
+var test = require('tape');
+var path = require('path');
+var fs = require('fs');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var TemplateLiteralCompile = require('../../index');
+
+var dist = path.resolve(__dirname, 'dist');
+var htmlTitle = 'this is title ' + Math.random();
+
+test('test simple webpack config', function (t) {
+  var config = {
+    output: {
+      filename: 'bundle.js',
+      path: dist,
+      publicPath: '/'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: '!!html-loader!index.html',
+        templateData: {
+          title: htmlTitle
+        }
+      }),
+      new TemplateLiteralCompile()
+    ]
+  };
+  webpack(config, function(err, stats) {
+    t.equal(err, null);
+    t.equal(stats.hasErrors(), false);
+    var htmlContent = fs.readFileSync(path.join(dist, 'index.html'), 'utf8');
+    var ret = htmlContent.match(/<title>(.+)<\/title>/);
+    t.equal(Array.isArray(ret), true);
+    t.equal(ret[1], htmlTitle);
+    ret = htmlContent.match(/rel="preload" href="(.+)"/);
+    t.equal(Array.isArray(ret), true);
+    t.equal(ret[1], config.output.publicPath + config.output.filename);
+    t.end();
+  });
+});
+
